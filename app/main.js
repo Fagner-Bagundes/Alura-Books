153a876b487d73010fa73351647d97ded401b7d9
Fagner_0 <ferreirabagundes@gmail.com>
2024-05-12 14:57:04 -0300

    .

--- app/main.js
+++ app/main.js
@@ -1,34 +1,10 @@
 let livros = []
-const endpointDaAPI = `https://guilhermeonrails.github.io/casadocodigo/livros.json`
-getBuscarLivrosDaAPI() 
-const elementoParaInserirLivros = document.querySelector(`.livros`)
+const endpointDaAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json'
+getBuscarLivrosDaAPI()
 
-async function getBuscarLivrosDaAPI () {
+async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointDaAPI)
     livros = await res.json()
-    console.table(livros);
-    exibirOsLivrosNaTela(livros)
-}
-
-function exibirOsLivrosNaTela(ListaDeLivros) {
-    ListaDeLivros.forEach(livros => {
-        elementoParaInserirLivros.innerHTML += 
-        `
-        <div class="livro">
-        <img class="livro__imagens" src= "${livros.imagem}"
-          alt="${livros.alt}" />
-        <h2 class="livro__titulo">
-        ${livros.titulo}
-        </h2>
-        <p class="livro__descricao">${livros.autor}</p>
-        <p class="livro__preco" id="preco">${livros.preco}</p>
-        <div class="tags">
-          <span class="tag">${livros.categoria}</span>
-          <span class="tag">${livros.categoria}</span>
-        </div>
-      </div>
-        `
-        
-    });
-    
+    livros = aplicarDesconto(livros)
+    exibirOsLivrosNaTela(livrosComDesconto)
 }
