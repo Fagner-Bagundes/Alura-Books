e46594cfac2f05ecec0a4ba0b65f4dba094c4296
Fagner_0 <ferreirabagundes@gmail.com>
2024-05-16 16:15:12 -0300

    .

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -6,4 +6,14 @@ function filtrarLivros() {
     const categoria = elementoBtn.value
     let livrosFiltrados = categoria == `disponivel` ? livros.filter(livro => livro.quantidade>0) : livros.filter(livro => livro.categoria == categoria)
     exibirOsLivrosNaTela(livrosFiltrados)
-}
+
+    if (categoria == `disponivel`) {
+        exibirValorTotalDosLivrosNaTela ()
+    }
+
+    function exibirValorTotalDosLivrosNaTela () {
+        elementoComValorTotalDeLivrosDisponiveis.innerHTML = ` <div class="livros__disponiveis">
+        <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+      </div>`
+    }
+ }
